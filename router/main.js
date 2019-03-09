e7c41fd0ebd4a49a5a8bac21d401fcf1d8543495
b0ho <ybg4828@gmail.com>
2019-03-10 04:08:06 +0900

    파파고에서 번역 한뒤 alpha 페이지로 번역된 단어 전송 완료

--- router/main.js
+++ router/main.js
@@ -3,13 +3,30 @@ var randomWords = require('random-words');
 words = randomWords(5, { min: 3, max: 10 })
 console.log(words);
 
+var client_id = 'MqIs5qaBHMxGw2g8roJ5';//개발자센터에서 발급받은 Client ID
+var client_secret = 'HEsxkBPZoY'; //개발자센터에서 발급받은 Client Secret
+var query = "whale";
+var api_url = 'https://openapi.naver.com/v1/language/translate';
+var request = require('request');
+var options = {
+    url: api_url,
+    form: {'source':'en', 'target':'ko', 'text':query},
+    headers: {'X-Naver-Client-Id':client_id, 'X-Naver-Client-Secret': client_secret}
+};
 
 module.exports = function (app, fs) {
+
   app.get("/alpha", function (req, res) {
-    res.render("index4.html", {
-      words : words
+    request.post(options, function (error, response, body) {
+      var n = JSON.parse(body).message.result.translatedText;
+      res.render("index4.html", {
+        words: words,
+        trans: n
+      })
     });
-  });
+}) 
+   
+    
   app.get("/", function (req, res) {
     res.render("index.html");
   });
---
+++ test.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+var client_id = 'MqIs5qaBHMxGw2g8roJ5';//개발자센터에서 발급받은 Client ID
+var client_secret = 'HEsxkBPZoY'; //개발자센터에서 발급받은 Client Secret
+var query = "hi";
+app.get('/translate', function (req, res) {
+   var api_url = 'https://openapi.naver.com/v1/language/translate';
+   var request = require('request');
+   var options = {
+       url: api_url,
+       form: {'source':'en', 'target':'ko', 'text':query},
+       headers: {'X-Naver-Client-Id':client_id, 'X-Naver-Client-Secret': client_secret}
+    };
+   request.post(options, function (error, response, body) {
+       res.writeHead(200, {'Content-Type': 'text/json;charset=utf-8'});
+         res.end(body);
+   });
+ });
+ app.listen(3001, function () {
+   console.log('http://127.0.0.1:3001/translate app listening on port 3000!');
+ });
