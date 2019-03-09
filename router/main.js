75bb092c8f038b907f17223ce7ac02030a5bdfcb
b0ho <ybg4828@gmail.com>
2019-03-10 02:29:10 +0900

    라우터에서 뷰로 값 전달 성공

--- router/main.js
+++ router/main.js
@@ -1,8 +1,16 @@
-module.exports = function(app) {
-  app.get("/alpha", function(req, res) {
-    res.render("index4.html");
+
+var randomWords = require('random-words');
+words = randomWords(5, { min: 3, max: 10 })
+console.log(words);
+
+
+module.exports = function (app, fs) {
+  app.get("/alpha", function (req, res) {
+    res.render("index4.html", {
+      words : words
+    });
   });
-  app.get("/", function(req, res) {
+  app.get("/", function (req, res) {
     res.render("index.html");
   });
   app.get("/cube", function(req, res) {
--- server.js
+++ server.js
@@ -13,5 +13,5 @@ app.set("view engine", "ejs");
 app.engine("html", require("ejs").renderFile);
 
 var server = app.listen(3000, function() {
-  console.log("Express server has started on port 3000");
+  console.log("http://localhost:3000");
 });
