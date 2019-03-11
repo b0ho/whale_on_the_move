24bf77375b05d6cc13f620a912ca29dc59c267e0
b0ho <ybg4828@gmail.com>
2019-03-12 02:12:44 +0900

    단어장 엑셀을 파싱하여 보낸 뒤 각 각의 배열로 생성함

--- router/main.js
+++ router/main.js
@@ -4,13 +4,24 @@ var XLSX = require("xlsx");
 var workbook = XLSX.readFile("./asset/wordbook.xlsx");
 var firstSheetName = workbook.SheetNames[0];
 var firstSheet = workbook.Sheets[firstSheetName];
-var set = XLSX.utils.sheet_to_json(firstSheet);
-var alphaset = JSON.stringify(set);
+var set = XLSX.utils.sheet_to_json(firstSheet, { raw: true });
+
+var alph = []
+var phon = []
+var han = []
+
+for (var i in set) {
+  alph[i] = set[i].s_alpha;
+  phon[i] = set[i].s_phonetic;
+  han[i] = set[i].s_hangeul;
+}
 
 module.exports = function(app, fs) {
   app.get("/alpha", function(req, res) {
     res.render("index4.html", {
-      alphaset: set
+      alph:alph,
+      phon:phon,
+      han:han
     });
   });
 
