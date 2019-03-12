e248eee9e8d51bc22aad06f379a0644bc4c73b07
b0ho <ybg4828@gmail.com>
2019-03-12 21:44:04 +0900

    버튼 이벤트 오류 해결 및 파일 정리 파비콘, 배경 완료

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ var app = express("./router/main");
 var router = require("./router/main")(app);
 
 app.use(express.static("node_modules"));
-app.use("/public", express.static(__dirname + "/public"));
 app.use("/asset", express.static(__dirname + "/asset"));
+app.use("/public", express.static(__dirname + "/public"));
+app.use("/script", express.static(__dirname + "/script"));
 app.use("/scripts", express.static(__dirname + "/node_modules/"));
 
 app.set("views", __dirname + "/views");
